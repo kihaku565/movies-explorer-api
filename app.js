0836ac73d0246a78264d0eae2295a0ee9078772f
kihaku565 <vas.matyushkin@yandex.ru>
2023-07-08 00:00:01 +0700

    Замени сообщения ответов и ошибок на константы

--- app.js
+++ app.js
@@ -3,31 +3,26 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
-const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
+const { limiter, devDatabaseUrl } = require('./utils/config');
 const router = require('./routes/routes');
 const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000 } = process.env;
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // за 15 минут
-  max: 1000, // можно совершить максимум 1000 запросов с одного IP
-});
+const { PORT = 3000, NODE_ENV, DATABASE_URL } = process.env;
 
 const app = express();
 
-app.use(cors());
+app.use(requestLogger);
 
-app.use(helmet()); // настраиваем заголовки
+app.use(limiter); // подключаем rate-limiter
+
+app.use(cors());
 
 app.use(bodyParser.json()); // для собирания JSON-формата
 
-app.use(requestLogger); // подключаем логгер запросов
-
-app.use(limiter); // подключаем rate-limiter
+app.use(helmet()); // настраиваем заголовки
 
 app.use(router);
 
@@ -36,6 +31,6 @@ app.use(errorLogger); // подключаем логгер ошибок
 app.use(errors()); // обработчик ошибок celebrate
 app.use(errorHandler); // мидлвара централизованного обработчика ошибок
 
-mongoose.connect('mongodb://127.0.0.1/moviesdb');
+mongoose.connect(NODE_ENV === 'production' ? DATABASE_URL : devDatabaseUrl);
 
 app.listen(PORT);
